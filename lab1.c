c12cb52772365b6eb10cb3d2ce980e94d5bc74ee
Arthur Pinheiro <114116008+vuvu43@users.noreply.github.com>
2024-03-27 16:00:40 -0300

    Update and rename main.c to lab1.c
    Upei o arquivo errado também, corrigindo...

--- lab1.c
+++ lab1.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
+#include <math.h>
 
-#define MTHREADS 3
+#define MTHREADS 6
 
 //vetor teste
-int vet[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-int N = 11;
+int vet[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+int vet_teste[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+int N = sizeof(vet) / sizeof(int);
 
 struct t_args{
-    int* vet;
     int N;
     int ini;
     int fim;
@@ -29,12 +29,25 @@ void* quadVet(void* args){
     //eleva ao quadrado elementos no intervalo [ini, fim-1]
     struct t_args arg = *(struct t_args*) args;
     
-    if (arg.fim > arg.N) arg.fim = arg.N; // corrigindo caso o .fim seja maior que o Nanho do vetor
-
+    if (arg.fim > arg.N) arg.fim = arg.N; // corrigindo caso o .fim seja maior que o tamanho do vetor
+    
+    //debug printf("[%d, %d]", arg.ini, arg.fim-1);
+    
     for (int i=arg.ini; i<arg.fim; i++){
         vet[i] *= vet[i];
     }
 
+}
+
+void func_teste(){
+    
+    for (int i=0; i<N; i++){
+        if (sqrt(vet[i]) != vet_teste[i]){
+            printf("\nInd> %d\nsqrt> %f\norig> %d\n", i, sqrt(vet[i]), vet_teste[i]);
+            puts("--ERRO: vetores diferentes"); exit(-1);
+        }
+    }
+    printf("\nTESTE OK :)");
 }
 
 int main(int argc, char** argv){
@@ -48,18 +61,20 @@ int main(int argc, char** argv){
         
         //inicializa argumentos
         struct t_args* args = malloc(sizeof(struct t_args));
-        args->vet = vet;
         args->N = N;
-        args->ini = i * N/MTHREADS;
+        args->ini = i * (N/MTHREADS);
         args->fim = (i + 1) * (N/MTHREADS);
         
         if( i == MTHREADS-1) args->fim = N; 
         
+        //código da prof
+        //cria as  threads
         if (pthread_create(&tid_sistema[i], NULL, quadVet, (void*) args)) {
             printf("--ERRO: pthread_create()\n"); exit(-1);
         }
     }
     
+    //código da prof
     //espera as threads terminarem
     for (int i=0; i<MTHREADS; i++) {
         if (pthread_join(tid_sistema[i], NULL)) {
@@ -70,5 +85,6 @@ int main(int argc, char** argv){
     //checa resultado
     puts("Vetor alterado: ");
     imprimeVet(vet, N);
+    func_teste();
     return 0;
 }
